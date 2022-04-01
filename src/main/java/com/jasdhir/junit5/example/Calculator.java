1dc5fb73b501c2f349d3f8293c46da8bffba9fb6
jasdhiratrevature <jasdhir.singh@revature.com>
2022-04-01 14:58:34 -0400

    Added the Main Class

--- src/main/java/com/jasdhir/junit5/example/Calculator.java
+++ src/main/java/com/jasdhir/junit5/example/Calculator.java
@@ -1,22 +1,19 @@
 package com.jasdhir.junit5.example;
 
 public class Calculator {
-	
-
-
-	public  int add(int i, int j) {
+	public int add(int i, int j) {
 		return i + j;
 	}
-	
-	public  int subtract(int i, int j) {
+
+	public int subtract(int i, int j) {
 		return i - j;
 	}
-	
-	public  int multiply(int i, int j) {
+
+	public int multiply(int i, int j) {
 		return i * j;
 	}
-	
-	public  int divide(int i, int j) {
+
+	public int divide(int i, int j) {
 		return i / j;
 	}
 }
